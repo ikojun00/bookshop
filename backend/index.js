9afa2d5151de775efa295a8d5988a99c504b510b
ikojun00 <ikojun00@fesb.hr>
2023-08-03 17:16:05 +0200

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "test"
+})
+
+app.get("/", (req, res) => {
+    const q = "SELECT * FROM books";
+    db.query(q, (err, data) => {
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.listen(8080, () => {
+    console.log("Connected to 8080...")
+})
