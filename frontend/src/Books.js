efe7ca0a1733f227eacac29273187054a481f563
ikojun00 <ikojun00@fesb.hr>
2023-08-03 18:55:08 +0200

    styling

--- frontend/src/Books.js
+++ frontend/src/Books.js
@@ -19,7 +19,20 @@ const Books = () => {
     console.log(books);
 
     return (
-        <div>Books</div>
+        <>
+        <div className="header">
+            <h1>Ordinary Bookshop</h1>
+        </div>
+        <div className="books">
+            {books.map((book) => (
+            <div key={book.id} className="book">
+                <img src={book.cover} alt={book.title} />
+                <h2>{book.title}</h2>
+                <p>{book.desc}</p>
+            </div>
+            ))}
+        </div>
+        </>
     )
 }
 
